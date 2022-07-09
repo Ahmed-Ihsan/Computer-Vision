7f032ec7ca2e9a3ad17beb5cdc50cc764b55ed17
Ahmed Ihsan Ali <46008612+Ahmed-Ihsan@users.noreply.github.com>
2022-07-09 12:35:33 +0300

    Add files via upload

--- Object Tracking/trackingV2.py
+++ Object Tracking/trackingV2.py
@@ -3,13 +3,13 @@ import cv2
 # from threading import Thread
 
 def resize(img):
-        return cv2.resize(img,(512,512))
+        return cv2.resize(img,(500,500))
 
 video = cv2.VideoCapture(0)
 ok,frame=video.read()
 frame = resize(frame)
 
-def draw(frame , x=0 , y=0 , n=0 , l= 0):
+def draw(frame , x=0 , y=0 , n=1, l= 1):
     p1 =  int(x) , int(y-30)
     frame = cv2.rectangle(frame,(p1),(p1[0]+1,p1[1]+60),(0,0,255),-1)
     p1 =  int(x-30) , int(y)
@@ -17,7 +17,9 @@ def draw(frame , x=0 , y=0 , n=0 , l= 0):
     p1 =  int(frame.shape[1]/2) , int(frame.shape[0]/2)
     frame = cv2.circle(frame,(p1),3,(0,0,255),-1)
     p1 =  int(frame.shape[1]/2) , int(frame.shape[0]/2)
-    frame = cv2.circle(frame,(p1),6,(0,0,255),2)
+    frame = cv2.circle(frame,(p1),45,(0,0,255),2)
+    frame = cv2.circle(frame,(p1),30,(255,255,255),2)
+    frame = cv2.circle(frame,(p1),15,(0,255,0),2)
     p1 =  [0 , 0]
     p2 = 0
     cont = 0
@@ -29,8 +31,15 @@ def draw(frame , x=0 , y=0 , n=0 , l= 0):
             fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.3, color=(0, 255, 255),thickness=1)
     for i in range(int(frame.shape[1]/10)):
         cont+=1
-        frame = cv2.rectangle(frame,(p1),(p1[0]+1,0+x),(255,255,255),-1)
-        frame = cv2.rectangle(frame,(x,p2),(0,p2+1),(255,255,255),-1)
+        if i == 25:
+            frame = cv2.rectangle(frame,(p1[0],p1[1]+60),(p1[0]+3,250),(0,0,255),-1)
+            frame = cv2.rectangle(frame,(250,p2),(60,p2+3),(0,0,255),-1)
+
+            frame = cv2.rectangle(frame,(p1),(p1[0],x),(0,0,255),-1)
+            frame = cv2.rectangle(frame,(x,p2),(0,p2),(0,0,255),-1)
+        else:
+            frame = cv2.rectangle(frame,(p1),(p1[0],x),(255,255,255),-1)
+            frame = cv2.rectangle(frame,(x,p2),(0,p2),(255,255,255),-1)
         if cont == 5:
             cont = 0
             x = 25
@@ -42,7 +51,7 @@ def draw(frame , x=0 , y=0 , n=0 , l= 0):
         else:
             x = 15
         p1[0] += 10
-        p2 += 10 
+        p2 += 10
     index= 0
     return frame
 
@@ -54,7 +63,6 @@ def click_event(event, x, y, flags, params):
     global pos_x , pos_y , frame_copy ,speed
     pos_x = x
     pos_y = y
-    print(event,flags)
     if event == 1:
         for i in range(1):
             dataObj[f"Tracker{i}"] = cv2.TrackerKCF_create()
@@ -65,13 +73,12 @@ def click_event(event, x, y, flags, params):
             speed = 0.1
         else:
             speed += 0.1
-        print(speed)
 
 pos_x , pos_y= frame.shape[0],frame.shape[1]
-n2 = 0
-l2=0
-l=0
-n = 0
+n2 = 1
+l2=1
+l=1
+n = 1
 while 1:
     k,frame=video.read()
     frame = resize(frame)
@@ -90,11 +97,9 @@ while 1:
         n2 -= speed
     elif pos_x  > frame.shape[1]/2 +  frame.shape[1]/3 :
         n2 += speed
-    
     if n2 == 360 or n2 == -360 :
         n2=0
     n = int(n2)
-
     if pos_y  < frame.shape[0]/2 - frame.shape[0]/3 :
         l2 -= speed
     elif pos_y  > frame.shape[0]/2 +  frame.shape[0]/3 :
